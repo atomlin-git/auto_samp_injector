796d8372b2ac997c27a7970c52ce2fe0cbe85c6b
atomlin-git <109459940+atomlin-git@users.noreply.github.com>
2024-12-06 17:29:37 +0300

    more beauty

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,6 @@
 #include "injector.hpp"
 
 injector ptr;
-
 injector::injector() {
     if(!std::string(GetCommandLineA()).contains("-auto")) return;
     if(!std::filesystem::exists("samp.dll")) return;
